1a39405619b26a057d2471e55454b84ea41ae28b
alfredz0007 <tumualfredz@gmail.com>
2022-09-27 01:12:46 -0700

    commits

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies a memory area
+ * @dest: memory area to be copied to
+ * @src: memory area to be copied from
+ * @n: number of bytes to be copied
+ *
+ * Return: pointer to the copied memory block
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int i = o;
+
+	for (: i < n; i++)
+	{
+		desy[i] = src[i];
+	}
+
+	return (dest);
+}
