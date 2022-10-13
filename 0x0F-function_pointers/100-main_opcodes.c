ee7cfd9a3cdb153d9f1f0aaa9d2520b64a04f3d6
alfredz0007 <tumualfredz@gmail.com>
2022-10-12 20:44:42 -0700

    commits

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -5,9 +5,9 @@
  * print_opcodes - print the opcodes of this program
  * @a: address of the main function
  * @n: number of bytes to print
- *
  * Return: void
  */
+
 void print_opcodes(char *a, int n)
 {
 	int i;
@@ -19,4 +19,29 @@ void print_opcodes(char *a, int n)
 			printf(" ");
 	}
 	printf("\n");
+}
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
+ * Return: always O
+ */
+int main(int argc, char **argv)
+{
+	int n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, n);
+	return (0);
 }
